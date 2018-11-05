36de82a85366978fd160847fefa3ee14d4e501da
Marcos Ferreira <marcosferreira100@hotmail.com>
2018-11-05 09:35:37 -0200

    Atualizando Classe Conta Corrente

--- AtividadeH2/src/Model/ContaCorrente.java
+++ AtividadeH2/src/Model/ContaCorrente.java
@@ -1,5 +1,12 @@
 package Model;
 
-public class ContaCorrente {
+public class ContaCorrente extends ContaBancaria {
+	public int valorLimite;
+	public boolean statusCont;
+	
+	public ContaCorrente(String titular2, int senha2, int numero2,int limiteCC) {
+		super(titular2, senha2, numero2);
+		this.valorLimite = limiteCC;
+	}
 
 }
