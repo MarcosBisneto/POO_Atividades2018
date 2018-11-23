a40be46522cd49e0f5bd022b5a578f0254eb8bd6
Marcos Ferreira <marcosferreira100@hotmail.com>
2018-11-23 02:13:24 -0300

    Fazendo o Menu

--- Atividade K - Oficial/src/app/Aplicação.java
+++ Atividade K - Oficial/src/app/Aplicação.java
@@ -13,89 +13,8 @@ import java.util.Scanner;
 
 public class Aplicação {
 	private static Scanner input = new Scanner(System.in);
-    private static boolean logado = false;
+    private static boolean logadoStatus = false;
     private static Processos processo = new Processos();
-    
-    /* Funções */
-    public static void main(String[] args) {
-        int opcao = 1;
-        
-        while (opcao != 0){
-            opcao = menu();
-            
-            if (logado){
-                switch (opcao) {
-                    case 1:
-                        criarQuadro();
-                        break;
-                    case 2:
-                        listarQuadros();
-                        break;
-                    case 3:
-                        selecionarQuadro();
-                        break;
-                    case 4:
-                        criarTarefa();
-                        break;
-                    case 5:
-                        listarTarefas();
-                        break;
-                    case 6:
-                        selecionarTarefa();
-                        break;
-                    case 7:
-                        criarCartao();
-                        break;
-                    case 8:
-                        listagemCompletaDeCartoes();
-                        break;
-                    case 9:
-                        selecionarCartao();
-                        break;
-                    case 10:
-                        adicionarDescricao();
-                        break;
-//                    case 11:
-//                        adicionarDataDeEntrega();
-//                        break;
-                    case 12:
-                        adicionarComentario();
-                        break;
-                    case 13:
-                        adicionarEtiqueta();
-                        break;
-                    case 14:
-                        moverCartao();
-                        break;
-                    case 15:
-                        mostrarLog();
-                        break;
-                    case 16:
-                        logoff();
-                    case 0:
-                        System.out.println("Sistema finalizado!");
-                        break;
-                    default:
-                        System.out.println("Opcao invalida!");
-                        break;
-                }
-            } else {
-                switch (opcao) {
-                    case 1:
-                        login();
-                        break;
-                    case 2:
-                        cadastarUsuario();
-                        break;
-                    case 0:
-                        System.out.println("Sistema finalizado!");
-                    default:
-                        System.out.println("Opcao invalida!");
-                        break;
-                }
-            }
-        }
-    }
     
     private static void criarQuadro(){
         input = new Scanner(System.in);
@@ -108,10 +27,10 @@ public class Aplicação {
     private static void listarQuadros(){
         ArrayList<Quadro> quadros = processo.getQuadrosCadastrados();
         if (quadros.isEmpty()){
-            System.out.println("Nenhum quadro localizado!");
+            System.out.println("Não há quadros!");
         }
         for (int i = 0; i < quadros.size(); i++){
-            System.out.printf(">> Quadro %d - %s\n", i+1, quadros.get(i).getTitulo());
+            System.out.printf(">> %dº Quadro: %s\n", i+1, quadros.get(i).getTitulo());
         }
     }
     
@@ -122,11 +41,10 @@ public class Aplicação {
         System.out.print("Opcao: ");
         int opcao = input.nextInt();
         if (processo.selecionarQuadro(opcao-1)){
-            System.out.println("Alteracao aplicada com sucesso!");
+            System.out.println("Entrou...!");
+        } else {
+            System.out.println("Esse quadro não existe!");
         }
-//        } else {
-//            System.out.println("Problemas na selecao, tente novamente!");
-//        }
     }
     
     
@@ -152,14 +70,11 @@ public class Aplicação {
     private static void selecionarTarefa(){
         input = new Scanner(System.in);
         listarTarefas();
-        System.out.print("Opcao: ");
+        System.out.print(">> Opcao: ");
         int opcao = input.nextInt();
         if (processo.selecionarTarefa(opcao-1)){
-            System.out.println("Alteracao aplicada com sucesso!");
+            System.out.println("Entrou...");
         }
-//        } else {
-//            System.out.println("Problemas na selecao, tente novamente!");
-//        }
     }
     
     
@@ -175,34 +90,32 @@ public class Aplicação {
         ArrayList<Card> cards = processo.getCartoesCadastrados();
         if (cards.isEmpty()){
             System.out.println("Nenhum card localizado!");
-        }
+        }else {
         for (int i = 0; i < cards.size(); i++){
-            System.out.printf("Card %d - %s\n", i+1, cards.get(i).getTitulo());
+            System.out.printf("%dº Card: %s\n", i+1, cards.get(i).getTitulo());
         }
+      }
     }
     
     
     private static void listagemCompletaDeCartoes(){
-        ArrayList<Card> cartoes = processo.getCartoesCadastrados();
-        if (cartoes.isEmpty()){
-            System.out.println("Nenhum registro localizado!");
+        ArrayList<Card> cards = processo.getCartoesCadastrados();
+        if (cards.isEmpty()){
+            System.out.println("Não há Cards!");
         }
-        for (int i = 0; i < cartoes.size(); i++){
-            System.out.printf("Titulo - %s\n", cartoes.get(i).getTitulo());
-            System.out.printf("Descricao - %s\n", cartoes.get(i).getDescricao());
+        for (int i = 0; i < cards.size(); i++){ 
+        	System.out.printf("%dº Card", i+1);
+            System.out.printf(">> Titulo - %s\n", cards.get(i).getTitulo());
+            System.out.printf(">> Descricao - %s\n", cards.get(i).getDescricao());
             
-            System.out.println("Comentarios");
-            for (int j = 0; j < cartoes.get(i).getComentarios().size(); j++){
-                System.out.printf("\t - %s - %s\n",
-                        cartoes.get(i).getComentarios().get(j).getComentario(),
-                        cartoes.get(i).getComentarios().get(j).getDataHora());
+            System.out.println("  > Comentario(s) Feito(s): ");
+            for (int j = 0; j < cards.get(i).getComentarios().size(); j++){
+                System.out.printf("\t - %s - %s\n",cards.get(i).getComentarios().get(j).getComentario(),cards.get(i).getComentarios().get(j).getDataHora());
             }
             
-            System.out.println("Etiquetas");
-            for (int j = 0; j < cartoes.get(i).getEtiquetas().size(); j++){
-                System.out.printf("\t - %s - %s\n", 
-                        cartoes.get(i).getEtiquetas().get(j).getTitulo(),
-                        cartoes.get(i).getEtiquetas().get(j).getCor());
+            System.out.println("  > Etiqueta(s) Aplicada(s): ");
+            for (int j = 0; j < cards.get(i).getEtiquetas().size(); j++){
+                System.out.printf("\t - %s - %s\n",cards.get(i).getEtiquetas().get(j).getTitulo(),cards.get(i).getEtiquetas().get(j).getCor());
             }
         }
     }
@@ -211,14 +124,11 @@ public class Aplicação {
     private static void selecionarCartao(){
         input = new Scanner(System.in);
         listarTarefas();
-        System.out.print("Opcao: ");
+        System.out.print(">> Opcao: ");
         int opcao = input.nextInt();
-        if (processo.selecionarCartao(opcao)){
-            System.out.println("Alteracao aplicada com sucesso!");
+        if (processo.selecionarCartao(opcao-1)){
+            System.out.println("Entrou...");
         }
-//        } else {
-//            System.out.println("Problemas na selecao, tente novamente!");
-//        }
     }
     
     
@@ -228,15 +138,6 @@ public class Aplicação {
         String descricao = input.nextLine();
         processo.adicionarDescricaoAoCartao(descricao);
     }
-    
-    
-//    private static void adicionarDataDeEntrega(){
-//        input = new Scanner(System.in);
-//        System.out.print("Data de entrega: ");
-//        String data = input.nextLine();
-//        processo.adicionarDataParaEntregaDoCartao(data);
-//    }
-//    
     
     private static void adicionarComentario(){
         input = new Scanner(System.in);
@@ -248,9 +149,9 @@ public class Aplicação {
     
     private static void adicionarEtiqueta(){
         input = new Scanner(System.in);
-        System.out.print("Etiqueta: ");
+        System.out.print(">> Titulo da Etiqueta: ");
         String titulo = input.nextLine();
-        System.out.print("Cor: ");
+        System.out.print(">> Cor da Etiqueta: ");
         String cor = input.nextLine();
         processo.adicionarEtiquetaAoCartao(titulo, cor);
     }
@@ -259,14 +160,14 @@ public class Aplicação {
     private static void moverCartao(){
         input = new Scanner(System.in);
         
-        System.out.println("Selecione a tarefa");
+        System.out.println(">> Selecione a tarefa");
         listarTarefas();
-        System.out.println("Opcao: ");
+        System.out.println(">> Opcao: ");
         int tarefa = input.nextInt();
         
-        System.out.println("Selecione o cartao");
+        System.out.println(">> Selecione o cartao");
         listarCartoes();
-        System.out.println("Opcao: ");
+        System.out.println(">> Opcao: ");
         int cartao = input.nextInt();
         
         processo.moverCartao(tarefa, cartao-1);
@@ -317,14 +218,14 @@ public class Aplicação {
         String senha = "";
         
         while (login.trim().length() == 0 || senha.trim().length() == 0){
-            System.out.print("Login: ");
+            System.out.print(">> Login: ");
             login = input.nextLine();
-            System.out.print("Senha: ");
+            System.out.print(">> Senha: ");
             senha = input.nextLine();
         }
         if (processo.login(login, senha)){
             System.out.println("Bem-Vindo!");
-            logado = true;
+            logadoStatus = true;
         } else {
             System.out.println("Ops!Você não foi localizado!");
         }
@@ -332,7 +233,7 @@ public class Aplicação {
     
     
     private static void logoff(){
-        logado = false;
+        logadoStatus = false;
         processo.selecionarUsuario(0);
     }
     
@@ -340,28 +241,32 @@ public class Aplicação {
         String menu;
         int opcao;
 
-        if (!logado){
-            menu = "1 - Login\n"
-                 + "2 - Cadastrar usuario\n"
-                 + "0 - Finalizar\n"
+        if (!logadoStatus){
+        	// seria o *PRIMEIRO MENU*
+            menu = "SEJA BEM VINDO - TRELLAR \n"
+            	 + "1 - Login\n" //se clicar nessa opçao vem o formaulario de LOGIN.
+                 + "2 - Cadastrar Usuario\n" //se clicar nessa opçao vem o formaulario de cadastro.
+                 + "0 - Finalizar\n" //sai.
                  + ">>> ";
         } else {
-            menu = "1 -  Criar novo quadro\n"
-                 + "2 -  Listar quadros\n"
-                 + "3 -  Selecionar quadro\n"
-                 + "4 -  Cadastrar tarefa\n"
-                 + "5 -  Listar tarefas\n"
+        	//seria o *SEGUNDO MENU* DEPOIS DEPOIS DE CLICAR NA OPÇÃO DA PRIMEIRA LOGIN 
+            menu = "1 -  Criar Novo Quadro\n" //se clicar nessa opçao vem o formaulario NOVO QUADRO.
+                 + "2 -  Meus Quadros - Listar Quadros\n" //tela com a lista.
+                 + "3 -  Selecionar Quadro\n"
+           //  SE CLICAR NA OPÇÃO MOSTRAR QUADROS: *TERCEIRO MENU* DEPOIS DE ENTRAR SELECIONAR O QUADRO.      
+                 + "4 -  Cadastrar tarefa\n" //se clicar nessa opçao vem o formaulario NOVO cadastrar tarefa.
+                 + "5 -  Listar tarefas\n" // listas tarefas. 
                  + "6 -  Selecionar tarefa\n"
-                 + "7 -  Adicionar novo cartao\n"
-                 + "8 -  Listar cartoes\n"
-                 + "9 -  Selecionar cartao\n"
-                 + "10 - Adicionar descrição ao cartao\n"
+
+            // DEPOIS DE SELECIONAR A TAREFA, APARECE O *QUARTO MENU*
+                 + "7 -  Adicionar Card\n"
+                 + "8 -  Listar Cards\n"      
+                 + "9 -  Selecionar Cards\n"
+            /// *QUINTO MENU* DEPOIS DE SELECIONAR UM CARD
+                 + "10 - Adicionar descrição ao card\n"
                  + "12 - Adicionar comentario ao cartao\n"
                  + "13 - Adicionar etiqueta ao cartao\n"
                  + "14 - Alternar cartao para outra tarefa\n"
-                 + "15 - Mostrar log\n"
-                 + "16 - Logoff\n"
-                 + "0 - Finalizar\n"
                  + ">>> ";
         }
         
@@ -369,6 +274,83 @@ public class Aplicação {
         System.out.print(menu);
         opcao = input.nextInt();
         return opcao;
+    }
+    
+    public static void main(String[] args) {
+        int opcao = 1;
+        
+        while (opcao != 0){
+            opcao = menu();
+            
+            if (logadoStatus){
+                switch (opcao) {
+                    case 1:
+                        criarQuadro();
+                        break;
+                    case 2:
+                        listarQuadros();
+                        break;
+                    case 3:
+                        selecionarQuadro();
+                        break;
+                    case 4:
+                        criarTarefa();
+                        break;
+                    case 5:
+                        listarTarefas();
+                        break;
+                    case 6:
+                        selecionarTarefa();
+                        break;
+                    case 7:
+                        criarCartao();
+                        break;
+                    case 8:
+                        listagemCompletaDeCartoes();
+                        break;
+                    case 9:
+                        selecionarCartao();
+                        break;
+                    case 10:
+                        adicionarDescricao();
+                        break;
+                    case 12:
+                        adicionarComentario();
+                        break;
+                    case 13:
+                        adicionarEtiqueta();
+                        break;
+                    case 14:
+                        moverCartao();
+                        break;
+                    case 15:
+                        mostrarLog();
+                        break;
+                    case 16:
+                        logoff();
+                    case 0:
+                        System.out.println("Sistema finalizado!");
+                        break;
+                    default:
+                        System.out.println("Opcao invalida!");
+                        break;
+                }
+            } else {
+                switch (opcao) {
+                    case 1:
+                        login();
+                        break;
+                    case 2:
+                        cadastarUsuario();
+                        break;
+                    case 0:
+                        System.out.println("Sistema finalizado!");
+                    default:
+                        System.out.println("Opcao invalida!");
+                        break;
+                }
+            }
+        }
     }
     
 }
